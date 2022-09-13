127bcf47a7f27b46f308678d600a962918c57e48
Manank Patel <pmanank2005@gmail.com>
2022-09-13 10:36:20 +0530

    update

--- fish/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --wraps='exa al' --wraps='exa -al' --description 'alias ls=exa -al'
-  exa -al $argv; 
-end
--- waybar/waybar/modules/cputemp.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-echo -en "{\"text\":\""
-sensors | rg Core | awk '/^Core /{++r; gsub(/[^[:digit:]]+/, "", $3); s+=$3} END{PREC=2; printf "%.0f%s", s/(10*r), "°C"}' && echo -en "\"}"
